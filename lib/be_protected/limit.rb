5fc32e96ebb5a33965b80c394472ce54e860267f
MihailDV <mihaildv@gmail.com>
2014-04-16 16:26:39 +0300

    add update limit

--- lib/be_protected/limit.rb
+++ lib/be_protected/limit.rb
@@ -5,5 +5,14 @@ module BeProtected
       Response::Limit.new request(:post, "/limits", params)
     end
 
+    def update(uuid, params)
+      Response::Limit.new request(:post, resource_path(uuid), params)
+    end
+
+    private
+    def resource_path(uuid)
+      "/limits/#{uuid}"
+    end
+
   end
 end
--- spec/be_protected/limit_spec.rb
+++ spec/be_protected/limit_spec.rb
@@ -3,11 +3,12 @@ require 'shared_examples/responses'
 
 describe BeProtected::Limit do
   let(:header) { {'Content-Type' => 'application/json'} }
+  let(:credentials) { {auth_login: 'account_uuid', auth_password: 'account_login'} }
 
   describe ".create" do
     let(:params) { {key: "USD_ID", volume: 1000, count: 145, max: 45} }
     let(:limit) do
-      described_class.new do |builder|
+      described_class.new(credentials) do |builder|
         builder.adapter :test do |stub|
           stub.post('/limits', params.to_json)  { |env| [status, header, response] }
         end
@@ -21,6 +22,35 @@ describe BeProtected::Limit do
       let(:response) { params.update(uuid: "LIMUUID").to_json }
 
       its(:status) { should == 201 }
+      its(:uuid)   { should == params[:uuid] }
+      its(:key)    { should == params[:key] }
+      its(:max)    { should == params[:max] }
+      its(:count)  { should == params[:count] }
+      its(:volume) { should == params[:volume] }
+      it_behaves_like "successful response"
+    end
+
+    it_behaves_like "failed response"
+    it_behaves_like "unknown response"
+  end
+
+  describe ".update" do
+    let(:params) { {key: "EUR_ID", volume: 2000, count: 245, max: 55} }
+    let(:limit) do
+      described_class.new(credentials) do |builder|
+        builder.adapter :test do |stub|
+          stub.post('/limits/uuid5', params.to_json)  { |env| [status, header, response] }
+        end
+      end
+    end
+
+    subject { limit.update("uuid5", params) }
+
+    context "when response is successful" do
+      let(:status)   { 200 }
+      let(:response) { params.update(uuid: "uuid5").to_json }
+
+      its(:status) { should == 200 }
       its(:uuid)   { should == params[:uuid] }
       its(:key)    { should == params[:key] }
       its(:max)    { should == params[:max] }
